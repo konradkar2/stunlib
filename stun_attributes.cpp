c458c1ffdcc84ddc47273c9b78791aa6fefd12db
Konrad Karas <konradkar2@gmail.com>
2026-06-14 20:32:13 +0200

    add stun attributes (mapped-address, xor-mapped-address), add tests

---
+++ stun_attributes.cpp
@@ -0,0 +1,123 @@
+#include "stun_attributes.hpp"
+#include <cstdint>
+#include <cstring>
+
+namespace stun {
+
+MappedAddressAttribute MappedAddressAttribute::create_v4(uint16_t port,
+                                                         uint32_t address) {
+  MappedAddressAttribute ret{};
+
+  ret.type = 0;   // TODO: insert type
+  ret.length = 8; // padding(1) + familiy(1) + port(2) + ipv4(4)
+  ret.m_family = AdressFamily::IPv4;
+  ret.m_port = port;
+  ret.m_address.ipv4 = address;
+
+  return ret;
+}
+
+MappedAddressAttribute MappedAddressAttribute::create_v6(uint16_t port,
+                                                         uint32_t address[4]) {
+  MappedAddressAttribute ret{};
+
+  ret.type = 0;    // TODO: insert type
+  ret.length = 20; // padding(1) + familiy(1) + port(2) + ipv4(16)
+  ret.m_family = AdressFamily::IPv6;
+  ret.m_port = port;
+  memcpy(&ret.m_address.ipv6, address, sizeof(ret.m_address.ipv6));
+
+  return ret;
+}
+
+size_t MappedAddressAttribute::serialize(std::span<uint8_t> target) const {
+  size_t offset = 0;
+
+  uint8_t padding = 0;
+  std::memcpy(target.data() + offset, &padding, sizeof(padding));
+  offset += sizeof(padding);
+
+  uint8_t family = static_cast<uint8_t>(m_family);
+  std::memcpy(target.data() + offset, &family, sizeof(family));
+  offset += sizeof(family);
+
+  uint16_t port = htons(m_port);
+  std::memcpy(target.data() + offset, &port, sizeof(port));
+  offset += sizeof(port);
+
+  if (m_family == AdressFamily::IPv4) {
+    uint32_t address = htonl(m_address.ipv4);
+    std::memcpy(target.data() + offset, &address, sizeof(address));
+    offset += sizeof(address);
+  } else {
+    uint32_t address[4] = {};
+    for (int i = 0; i < 4; ++i) {
+      address[i] = htonl(m_address.ipv6[i]);
+    }
+    std::memcpy(target.data() + offset, &address, sizeof(address));
+    offset += sizeof(address);
+  }
+
+  return offset;
+}
+
+AdressFamily MappedAddressAttribute::getFamily() const{
+    return m_family;
+}
+
+XorMappedAddressAttribute XorMappedAddressAttribute::create_v4(uint16_t port, uint32_t address,
+                                          uint32_t magic_cookie) {
+  XorMappedAddressAttribute ret{};
+
+  ret.m_mapped_attribute = MappedAddressAttribute::create_v4(port, address);
+  ret.m_magic_cookie = magic_cookie;
+                                            
+  return ret;
+}
+
+XorMappedAddressAttribute XorMappedAddressAttribute::create_v6(uint16_t port, uint32_t address[4],
+                                          uint32_t magic_cookie,
+                                          uint32_t transaction_id[3]) {
+  XorMappedAddressAttribute ret{};
+
+  ret.m_mapped_attribute = MappedAddressAttribute::create_v6(port, address);
+  ret.m_magic_cookie = magic_cookie;
+  memcpy(ret.m_transaction_id, transaction_id, sizeof(m_transaction_id));                                   
+                                            
+  return ret;
+}
+
+size_t XorMappedAddressAttribute::serialize(std::span<uint8_t> target) const {
+  size_t size = m_mapped_attribute.serialize(target);
+
+  constexpr size_t kPortOffset = 2;
+  constexpr size_t kAddressOffset = 4;
+
+  uint32_t magic_cookie = htonl(m_magic_cookie);
+  const auto *magic_cookie_bytes =
+      reinterpret_cast<const uint8_t *>(&magic_cookie);
+
+  target[kPortOffset] ^= magic_cookie_bytes[0];
+  target[kPortOffset + 1] ^= magic_cookie_bytes[1];
+
+  if (m_mapped_attribute.getFamily() == AdressFamily::IPv4) {
+    for (size_t i = 0; i < sizeof(magic_cookie); ++i) {
+      target[kAddressOffset + i] ^= magic_cookie_bytes[i];
+    }
+  } else {
+    const auto *transaction_id_bytes =
+        reinterpret_cast<const uint8_t *>(m_transaction_id);
+
+    for (size_t i = 0; i < sizeof(magic_cookie); ++i) {
+      target[kAddressOffset + i] ^= magic_cookie_bytes[i];
+    }
+    for (size_t i = 0; i < sizeof(m_transaction_id); ++i) {
+      target[kAddressOffset + sizeof(magic_cookie) + i] ^=
+          transaction_id_bytes[i];
+    }
+  }
+
+  return size;
+}
+
+} // namespace stun
---
+++ tests/stun_attributes_test.cpp
@@ -0,0 +1,55 @@
+#include "stun_attributes.hpp"
+
+#include <array>
+#include <cstdint>
+
+#include <gtest/gtest.h>
+
+namespace stun {
+namespace {
+
+TEST(MappedAddressAttribute, SerializesIpv4Value) {
+  auto attr = MappedAddressAttribute::create_v4(0x1234, 0xC0000201);
+  std::array<uint8_t, 8> buffer{};
+
+  const size_t size = attr.serialize(buffer);
+
+  const std::array<uint8_t, 8> expected{
+      0x00, 0x01, 0x12, 0x34, 0xC0, 0x00, 0x02, 0x01};
+  EXPECT_EQ(size, expected.size());
+  EXPECT_EQ(buffer, expected);
+}
+
+TEST(MappedAddressAttribute, SerializesIpv6Value) {
+  std::array<uint32_t, 4> address{0x20010DB8, 0x00000000, 0x00000000,
+                                  0x00000001};
+  auto attr = MappedAddressAttribute::create_v6(0x1234, address.data());
+  std::array<uint8_t, 20> buffer{};
+
+  const size_t size = attr.serialize(buffer);
+
+  const std::array<uint8_t, 20> expected{
+      0x00, 0x02, 0x12, 0x34, 0x20, 0x01, 0x0D, 0xB8, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
+  EXPECT_EQ(size, expected.size());
+  EXPECT_EQ(buffer, expected);
+}
+
+TEST(XorMappedAddressAttribute, SerializesIpv4Value) {
+  auto attr = XorMappedAddressAttribute::create_v4(0x1234, 0xC0000201,
+                                                  kMagicCookie);
+  std::array<uint8_t, 8> buffer{};
+
+  const size_t size = attr.serialize(buffer);
+
+  // src:    00 01 12 34 C0 00 02 01
+  // mask:   00 00 21 12 21 12 A4 42
+  // result: 00 01 33 26 E1 12 A6 43
+  const std::array<uint8_t, 8> expected{
+      0x00, 0x01, 0x33, 0x26, 0xE1, 0x12, 0xA6, 0x43};
+  EXPECT_EQ(size, expected.size());
+  EXPECT_EQ(buffer, expected);
+}
+
+} // namespace
+} // namespace stun
